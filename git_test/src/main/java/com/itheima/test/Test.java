aeaa7ed50de78fa56cff4ec94d1201ae77310513
xjp <1603164237@qq.com>
2018-11-18 20:02:11 +0800

    把本地过程添加到git

---
+++ git_test/src/main/java/com/itheima/test/Test.java
@@ -0,0 +1,14 @@
+package com.itheima.test;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("测试git");
+        System.out.println("测试git");
+        System.out.println("测试git");
+        System.out.println("测试git");
+        System.out.println("测试git");
+    }
+
+
+
+}
